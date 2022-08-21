35833a995f6283f43e16407d2d83c359b87d2b7a
miskiabdirizak <miskiabdirizak02@gmail.com>
2022-08-21 19:34:12 -0400

    Updating sample

--- pythonSample.py
+++ pythonSample.py
@@ -1,3 +1,8 @@
 #Practice Python + GIT
+from cgi import print_arguments
+
+
+print("Name: Miski Abdirizak")
 print("hello world")
 print("learning how to use git, github, and vscode!")
+print("----------------------------------------------")
